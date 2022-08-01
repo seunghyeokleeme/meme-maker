bab8d3da3a7f000ace63f4e16bb64bcdd5e7e725
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-01 21:50:14 +0900

    Drawing Project Two

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 20);
+ctx.fillRect(210 - 40, 200 - 20, 15, 100);
+ctx.fillRect(350 - 40, 200 - 20, 15, 100);
+ctx.fillRect(260 - 40, 200 - 20, 60, 200);
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.arc(250, 100, 50, 0, 2 * Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 5, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 5, Math.PI, 2 * Math.PI);
+ctx.moveTo(230, 100);
+ctx.arc(250, 100, 20, 0, 0.2 * Math.PI);
 ctx.fill();
