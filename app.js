fcaf5c727379645ef4ff9f5c881423a31ed9fe1c
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-03 07:52:08 +0900

    Line width

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -11,6 +13,7 @@ function onMove(event) {
     ctx.stroke();
     return;
   }
+
   ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -20,9 +23,16 @@ function startPainting(event) {
 
 function cancelPainting(event) {
   isPainting = false;
+  ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
