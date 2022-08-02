a054ef6318d66111df02d35181b8a05ee2c6b8ab
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-03 07:03:44 +0900

    Painting Lines

--- app.js
+++ app.js
@@ -3,17 +3,17 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(210 - 40, 200 - 20, 15, 100);
-ctx.fillRect(350 - 40, 200 - 20, 15, 100);
-ctx.fillRect(260 - 40, 200 - 20, 60, 200);
+ctx.lineWidth = 2;
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
+const colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e"];
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(260 + 10, 80, 5, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 5, Math.PI, 2 * Math.PI);
-ctx.moveTo(230, 100);
-ctx.arc(250, 100, 20, 0, 0.2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("click", onClick);
