45d999626d9828737efc38c4e7e061061909a1c3
seunghyeokleeme <seunghyeok.tech@gmail.com>
2022-08-03 07:24:34 +0900

    Mouse Painting

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+let isPainting = false;
 
-const colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e"];
-
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("click", onClick);
+function startPainting(event) {
+  isPainting = true;
+}
+
+function cancelPainting(event) {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
